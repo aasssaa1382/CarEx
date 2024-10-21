dc90d744eea577c18e1f2c83bcf24b6f4eb3bb05
tekegahapp <tekegahapp@gmail.com>
2024-10-21 10:10:34 +0330

    Add project files.

---
+++ CarWeb/Components/Pages/Counter.razor.cs
@@ -0,0 +1,68 @@
+﻿
+using CarWeb.Services.Option;
+using System.Net.Http;
+using static CarWeb.Components.Pages.Counter;
+
+namespace CarWeb.Components.Pages
+{
+    public partial class Counter
+    {
+
+        private ApiResponse? drivers;
+        private HttpClient client=new();
+        protected override async Task OnInitializedAsync()
+        {
+            drivers = await LoadPage(1);
+        }
+
+
+        private async Task<ApiResponse> LoadPage(int pageIndex)
+        {
+            drivers= await client.GetFromJsonAsync<ApiResponse>($"{APIs.BaseUrl}{APIs.GetDrives}{pageIndex}&PageSize=5") ?? new();
+            StateHasChanged(); 
+            return drivers;
+        }
+
+        public class ApiResponse
+        {
+            public bool Result { get; set; }
+            public string? ErrorMessages { get; set; }
+            public string? ErrorMessagesIX { get; set; }
+            public Content? Content { get; set; }
+        }
+
+        public class Content
+        {
+            public int PageIndex { get; set; }
+            public int PageSize { get; set; }
+            public bool HasNext { get; set; }
+            public bool HasPrevious { get; set; }
+            public int TotalPages { get; set; }
+            public int TotalRecord { get; set; }
+            public List<Driver>? Data { get; set; }
+        }
+
+        public class Driver
+        {
+            public int DriverId { get; set; }
+            public string? NameFamily { get; set; }
+            public string? Expertise { get; set; }
+            public string? ImageProFile { get; set; }
+            public bool IsActive { get; set; }
+            public int ProvinceId { get; set; }
+            public int SeenCount { get; set; }
+            public string? CarriagePermit { get; set; }
+            public int Priority { get; set; }
+            public int Rank { get; set; }
+            public int Score { get; set; }
+            public string? CarTypeId { get; set; }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ CarWeb/Services/Option/APIs.cs
@@ -0,0 +1,8 @@
+﻿namespace CarWeb.Services.Option
+{
+    public class APIs
+    {
+        public const string BaseUrl = "https://test.cobi.ir/";
+        public const string GetDrives = "api/v1/WebRead/drivers/pagination-driver?PageIndex=";
+    }
+}
